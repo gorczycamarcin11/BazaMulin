265e582449c7b414cb5402ead523c4fae1b67247
Marcin <gorczycamarcin11@gmail.com>
2017-07-30 12:50:12 +0200

    initial commit

---
+++ src/main/java/com/example/BazaMulin/BazaMulinApplication.java
@@ -0,0 +1,12 @@
+package com.example.BazaMulin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazaMulinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazaMulinApplication.class, args);
+	}
+}
