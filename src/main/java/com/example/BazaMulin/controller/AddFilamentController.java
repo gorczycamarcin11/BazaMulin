a045a807450ad68791bf1a8d13e84dd1beb9be21
Marcin <gorczycamarcin11@gmail.com>
2017-07-31 16:12:11 +0200

    Improved add form

--- src/main/java/com/example/BazaMulin/controller/AddFilamentController.java
+++ src/main/java/com/example/BazaMulin/controller/AddFilamentController.java
@@ -7,7 +7,6 @@ import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
